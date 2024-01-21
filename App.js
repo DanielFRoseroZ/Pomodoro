3de22d0cc8abb213c86f31bee88422d5c7407ee2
Daniel Felipe Rosero Zemanate <dandan32099@gmail.com>
2024-01-21 17:29:54 -0500

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Platform, Text, View, Button, SafeAreaView, TouchableOpacity } from 'react-native';
+import { useState, useEffect } from 'react';
+import Header from './src/components/Header';
+import Timer from './src/components/Timer';
+import { Audio } from 'expo-av';
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs(['new NativeEventEmitter']); // Ignore log notification by message
+LogBox.ignoreAllLogs(); //Ignore all log notifications
+
+const colors = ["#c7f6d4", "#ededaf", "#ff9688"]
 
 export default function App() {
+  const [isWorking, setIsWorking] = useState(false);
+  const [time, setTime] = useState(25 * 60);
+  const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" | "BREAK");
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setTime(time - 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    if (time === 0) {
+      setIsActive(false);
+      setIsWorking((prev) => !prev);
+      setTime(isWorking ? 1500 : 300);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, time])
+
+  function handleStartStop () {
+    playSound();
+    setIsActive(!isActive);
+  }
+
+  async function playSound () {
+    const { sound } = await Audio.Sound.createAsync(
+      require("./assets/click.mp3")
+    );
+    await sound.playAsync();
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={[styles.container, {backgroundColor: colors[currentTime]}]}>
+      <View style={{ flex: 1, paddingHorizontal: 15, paddingTop: Platform.OS === "android" && 30,  }}>
+        <Text style={styles.text}>Pomodoro</Text>
+        <Header currentTime={currentTime} setCurrentTime={setCurrentTime} setTime={setTime}/>
+        <Timer time={time}/>
+        <TouchableOpacity onPress={handleStartStop} style={styles.button}>
+          <Text style={{color: "white", fontWeight: "bold" }}>{isActive ? "STOP" : "START"}</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  text: {
+    fontSize: 32, 
+    fontWeight: "bold" 
+  },
+  button: {
+    backgroundColor: "#333333",
+    width: "100%",
+    paddingVertical: 15,
+    alignItems: "center",
+    borderRadius: 10,
+    marginTop: 20
+  }
 });
---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { useState } from 'react';
+
+const options = ["Pomodoro", "Short Break", "Long Break"];
+
+export default function Header({currentTime, setCurrentTime, setTime}) {
+    function handlePress(index) {
+        const newTime = index === 0 ? 25 : index === 1 ? 5 : 15;
+        setCurrentTime(index);
+        setTime(newTime * 60);
+    }
+
+    return (
+        <View style={{flexDirection: "row"}}>
+            {options.map((option, index) => {
+                return (
+                    <TouchableOpacity key={index} onPress={() => handlePress(index)} style={[styles.itemStyle, currentTime !== index && {borderColor: "transparent"}]}>
+                        <Text style={{fontWeight: "bold"}}>{option}</Text>
+                    </TouchableOpacity>
+                )
+            })}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    itemStyle: {
+        borderWidth: 3,
+        padding: 5,
+        width: "33%",
+        alignItems: "center",
+        borderRadius: 10,
+        borderColor: "white",
+        marginVertical: 20,
+    },
+})
